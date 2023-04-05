6b873fcf652dfea6e04ad352b015c3f8627b2cca
ruthashaolu99@gmail.com <ruthashaolu99@gmail.com>
2023-04-04 20:46:26 -0700

    repush

--- app.py
+++ app.py
@@ -6,44 +6,12 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, sql
 import os
 import json 
-#IF YOU WANT TO USE SQL POSTGRES YOU CAN USE THIS CODE TO CONNECT TO SERVER
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
-# from flask import jsonify
-# engine = create_engine(
-#     "postgresql://postgres:Gallito123!@localhost:5433"
-# )
-# session = Session(engine)
-# 2. Create an app, being sure to pass __name__
-
-########################
-#IF YOU WANT TO USE MongoDB USE THIS CODE
-# Import dependencies
-# from pymongo import MongoClient
-#  # Create an instance of MongoClient
-# mongo = MongoClient(port=27017)
-# engines = create_engine("mysql+pymysql://user:pw@host/db", pool_pre_ping=True)
 
 app = Flask(__name__)
 
 alltables=create_engine("sqlite:///data/all_tables.db")
 
-
-
-# 3. Define what to do when a user hits the index route
-# @app.route("/")
-# def home():
-#     print("Server received request for 'Home' page...")
-#     return mdf.to_json()
-
-
-# # 4. Define what to do when a user hits the /about route
-# @app.route("/about")
-# def about():
-#     print("Server received request for 'About' page...")
-#     return render_template('index.html')
-
+#  1.Define what to do when a user hits the /api/missions route
 @app.route("/api/missions")
 def mission():
     connection=alltables.connect()
@@ -51,6 +19,7 @@ def mission():
     connection.close()
     return missions.to_json(orient="records")
 
+# 2.Define what to do when a user hits the /api/estancias route
 @app.route("/api/estancias")
 def estancias():
     connection=alltables.connect()
@@ -58,24 +27,15 @@ def estancias():
     connection.close()
     return estancias.to_json(orient="records")
 
+# 3.Define what to do when a user hits the /api/asistancias route
 @app.route("/api/asistencias")
 def asistencias():
     connection=alltables.connect()
     asistencias=pd.read_sql('select * from asistencias_data', con=connection)
     connection.close()
     return asistencias.to_json(orient="records")
-    
-# 4. Define what to do when a user hits the /about route
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return render_template('index.html')
 
-# engine=create_engine('sqlite:///data/missions_data.sqlite')
-    # mdf = pd.read_sql('select * from missions_df', engine)
-    # print("Server received request for 'About' page...")
-    # return mdf.to_json(orient="records")
-
+# 4.Define what to do when a user hits the /api/geojson/federal route   
 @app.route("/api/geojson/federal")
 def federal():
     print("inside federal")
@@ -83,6 +43,7 @@ def federal():
         data = json.load(f)
     return data
 
+# 5.Define what to do when a user hits the /api/geojson/historical route
 @app.route("/api/geojson/historical")
 def historical():
     print("inside federal")
@@ -90,12 +51,11 @@ def historical():
         data = json.load(f)
     return data
 
-# @app.route("/api/estancias")
-# def estancias():
-#     engine=create_engine('sqlite:///data/estancias_data.sqlite')
-#     edf = pd.read_sql('select * from estancias_df', engine)
-#     print("inside estancias")
-#     return edf.to_json(orient = "records")
+# 6. Define what to do when a user hits the /about route
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
