0ab6d10dfcca32889be0e67e5bd780936e8ab693
Sarah <116603987+samipete@users.noreply.github.com>
2023-04-05 11:09:37 -0700

    final readme and sqlite DB

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # project_home
 Full-stack data visualization web application to interactively explore a dataset.
+# Visualizing Populations of Indigenous California Tribes 
+***Project Description***: Full-stack data visualization web application to interactively explore a dataset using a multi-layered map.
+![map_screenshot](Images/map_screenshot.png)
+
+## Process
+1. The data was sourced from https://gis.data.ca.gov/datasets/CAEnergy::federally-recognized-tribal-lands/about and 
+https://hub.arcgis.com/datasets/univredlands::historic-native-american-territories-in-california-1/about. The data was extracted from these geoJSON files. 
+
+2. After extracting the data, transform the data using ipynb notebook. Create pandas dataframes with relevant columns and change coordinates to strings. Then, load the data into a master SQLite database that has tables for the missions, estancias, asitancias, reservations, federal land, and historical land.
+
+3. Use javascript to create a base map using open street map. Use functions to create layers for the missions, estancias, and asistancias that will show them on the map, and have informational popups.
+
+4. Create an index.html file that will house the code for the home page. This will contain the scripts that your javascript files will use as well as call your javascript files.
+
+5. Create a bar chart to visualize the difference in total land area from the historical data and the federally recognized data. Use Chart.js to create this visualization. 
+![barchart](Images/myChart.png)
+
+
+## Software/Libraries Used
+Juptyer Notebook, D3, Javascript, Chart.js, sqlalchemy, flask
+
+
+## Sources
+https://gis.data.ca.gov/datasets/CAEnergy::federally-recognized-tribal-lands/about
+
+https://hub.arcgis.com/datasets/univredlands::historic-native-american-territories-in-california-1/about
+
+https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.min.js
+
+https://nahc.ca.gov/
+
+http://www.wherearethechildren.ca 
+
+https://www.openstreetmap.org/copyright
+
--- app.py
+++ app.py
@@ -1,44 +1,68 @@
 
-from flask import Flask
-from pprint import pprint
+from flask import Flask, jsonify, render_template, send_from_directory
 import pandas as pd
-import flask
-#IF YOU WANT TO USE SQL POSTGRES YOU CAN USE THIS CODE TO CONNECT TO SERVER
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
-# from flask import jsonify
-# engine = create_engine(
-#     "postgresql://postgres:Gallito123!@localhost:5433"
-# )
-# session = Session(engine)
-# 2. Create an app, being sure to pass __name__
-
-########################
-#IF YOU WANT TO USE MongoDB USE THIS CODE
-# Import dependencies
-# from pymongo import MongoClient
-#  # Create an instance of MongoClient
-# mongo = MongoClient(port=27017)
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, sql
+import os
+import json 
 
 app = Flask(__name__)
 
+alltables=create_engine("sqlite:///data/all_tables.db")
 
+#  1.Define what to do when a user hits the /api/missions route
+@app.route("/api/missions")
+def mission():
+    connection=alltables.connect()
+    missions=pd.read_sql('select * from missions_data', con=connection)
+    connection.close()
+    return missions.to_json(orient="records")
 
-# 3. Define what to do when a user hits the index route
-@app.route("/")
-def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+# 2.Define what to do when a user hits the /api/estancias route
+@app.route("/api/estancias")
+def estancias():
+    connection=alltables.connect()
+    estancias=pd.read_sql('select * from estancias_data', con=connection)
+    connection.close()
+    return estancias.to_json(orient="records")
 
+# 3.Define what to do when a user hits the /api/asistancias route
+@app.route("/api/asistencias")
+def asistencias():
+    connection=alltables.connect()
+    asistencias=pd.read_sql('select * from asistencias_data', con=connection)
+    connection.close()
+    return asistencias.to_json(orient="records")
 
-# 4. Define what to do when a user hits the /about route
+# 4.Define what to do when a user hits the /api/geojson/federal route   
+@app.route("/api/geojson/federal")
+def federal():
+    print("inside federal")
+    with open(os.path.join("./Resources","Federal.geojson")) as f:
+        data = json.load(f)
+    return data
+
+# 5.Define what to do when a user hits the /api/geojson/historical route
+@app.route("/api/geojson/historical")
+def historical():
+    print("inside federal")
+    with open(os.path.join("./Resources","Historical.geojson")) as f:
+        data = json.load(f)
+    return data
+
+# 6. Define what to do when a user hits the /about route
 @app.route("/about")
 def about():
     print("Server received request for 'About' page...")
-    return flask.render_template('index.html')
+    return render_template('index.html')
 
+@app.route("/home")
+def home():
+    print("Server received request for 'Home' page...")
+    return render_template('index2.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
